58d32c2e262ea6994c1f0980b2f4658b06b61f55
Rube <353371737@qq.com>
2016-03-24 15:02:29 +0800

    add site fouces

---
+++ server/src/models/sitefocus.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2015-present, Rube Dong
+ * All rights reserved.
+ *
+ * This source code is licensed under the GPL-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import Sequelize from 'sequelize';
+import {getMysql} from '../services/cache';
+
+const db = getMysql();
+const Sitefocus = db.define('collection', {
+    id: {type: Sequelize.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+    collection_id: {type: Sequelize.STRING(64)},
+    collection_site: {type: Sequelize.STRING(64)},
+    collection_created: {type: Sequelize.DATE, defaultValue: Sequelize.NOW},
+    collection_desc: {type: Sequelize.STRING(128)},
+    collection_userId: {type: Sequelize.INTEGER},
+    collection_status: {type: Sequelize.INTEGER}
+}, {
+    freezeTableName: true,
+    timestamps: false
+}, {indexes: [{unique: true, fields: ['collection_id', 'collection_userId']}]});
+
+export default Sitefocus;
--- server/src/services/collect.js
+++ server/src/services/collect.js
@@ -8,6 +8,7 @@
  */
 import crypto from 'crypto';
 import Collection from '../models/collection';
+import SiteFocus from '../models/sitefocus';
 
 export const collected = function *() {
     "use strict";
@@ -29,7 +30,7 @@ export const collected = function *() {
         console.error('Error    ' + error);
         this.status = 500;
         this.body = {result: 0}
-    })
+    });
 };
 
 export const unCollected = function *() {
@@ -71,6 +72,91 @@ export const getCollections = function *() {
     "use strict";
     let user = this._domain.user;
     let collections = yield Collection.findAll({
+        where: {
+            collection_status: 1,
+            collection_userId: user.id
+        }
+    });
+    console.log(collections);
+    if (collections) {
+        this.body = {
+            result: 1,
+            collections: collections
+        }
+    } else {
+        console.error('Error    ' + 0);
+        this.status = 500;
+        this.body = {result: 0}
+    }
+};
+
+/** site **/
+
+export const collectedSite = function *() {
+    "use strict";
+    let user = this._domain.user;
+    let {site,desc} = this.request.body.fields;
+    let collection_id = user.id + '-' + site;
+    yield SiteFocus.upsert({
+        collection_id,
+        collection_userId: user.id,
+        collection_site: site,
+        collection_desc: desc,
+        collection_content: content,
+        collection_status: 1
+    }).then(collection=> {
+        this.body = {result: 1, id: collection_id}
+    }, error=> {
+        console.error('Error    ' + error);
+        this.status = 500;
+        this.body = {result: 0}
+    })
+};
+
+export const unCollectedSite = function *() {
+    "use strict";
+    let user = this._domain.user;
+    let site = this.request.body.fields.site;
+    yield SiteFocus.update({collection_status: 0}, {
+            where: {
+                collection_id: user.id + '-' + site,
+                collection_userId: user.id
+            }
+        })
+        .then(rs=> {
+            if (rs) {
+                this.body = {result: 1}
+            } else {
+                console.error('Error    ' + 0);
+                this.status = 500;
+                this.body = {result: 0}
+            }
+        }, error=> {
+            console.error('Error    ' + error);
+            this.status = 500;
+            this.body = {result: 0}
+        });
+};
+
+export const judgeSiteFocused = function *() {
+    "use strict";
+    let user = this._domain.user, site = this.request.body.fields.site;
+    let collection = yield SiteFocus.findOne({
+        where: {
+            collection_id: user.id + '-' + site,
+            collection_userId: user.id,
+            collection_status: 1
+        }
+    });
+    this.body = {
+        result: +(collection ? true : false)
+    }
+};
+
+export const getSitefocused = function *() {
+    "use strict";
+    let user = this._domain.user;
+    let collections = yield SiteFocus.findAll({
         where: {
             collection_status: 1,
             collection_userId: user.id
