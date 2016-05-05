c70d72879284960f157308e186c78f135298e643
Rube <353371737@qq.com>
2016-05-06 02:39:34 +0800

    工具提取

---
+++ server/src/tools/auth.js
@@ -0,0 +1,30 @@
+//请求形式校验 delete/put
+export const methodAuth = method => {
+    return function*(next) {
+        if (this.request.body.fields._method === method) {
+            if (next){
+                yield next;
+            } else {
+                return true;
+            }
+        } else {
+            throw new Error('methodAuth checkError 500');
+        }
+    }
+};
+
+//参数空校验
+export const blankAuth = (query, body, params) => {
+    return function*(next) {
+        if ((query && !query.filter(p => !!this.query[p]).length === query.length) ||
+            (body && !body.filter(b => !!this.request.body.fields[b]) === body.length) ||
+            (params && !params.filter(p => !!this.params[p]) === params.length)) {
+            throw new Error('blankAuth checkError 500');
+        }
+        if (next){
+            yield next;
+        } else {
+            return true;
+        }
+    }
+}
