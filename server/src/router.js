b7b76f2713a4f16a57298d34684ab44f2c4d64c7
Rube <353371737@qq.com>
2016-05-09 02:41:52 +0800

    全新的路由地址

--- server/src/router.js
+++ server/src/router.js
@@ -9,161 +9,119 @@
 
 import fs from 'fs';
 import path from 'path';
+import {blankAuth, methodAuth} from './tools/auth';
+import * as siteController from './controllers/site';
+import * as searchController from './controllers/search';
+import * as userController from './controllers/user';
+import * as collectController from './controllers/collect';
+import * as recommendController from './controllers/recommend';
 import * as _controller from './controller';
 import * as searchService from './services/search';
-import * as loginService from './services/login';
 import * as collectService from './services/collect';
+import log from './services/log';
 
 const SITES = fs.readdirSync(`${__dirname}/sites/`);
 
 export default (router)=> {
     "use strict";
 
-    var error_auth = function() {
-        this.body = 'Auth Error';
-        this.status = 403;
-    };
+    router.use('/', log);
 
-    var methodAuth = method => {
-        return function *(next) {
-            if (this.request.body.fields._method === method) {
-                yield next;
-            } else {
-                error_auth.call(this);
-            }
-        }
-    };
+    router.post('/search', blankAuth(null, ['type']), searchController.searchEntrance);
 
-    router.use('/:site', function *(next) {
+    router.post('/collect/site', blankAuth(null, ['site', 'token']), userController.auth, collectController.collectSite);
 
-        let site = this.params.site;
-        let type = site.substr(site.split('-').shift().length + 1);
-        if (type !== 'bbs' && type !== 'blog'){
-            this._domain = this._domain || {};
-            this._domain.pluginName = type;
-        }
-        yield next;
-    });
+    router.delete('/collect/site', blankAuth(null, ['site', 'token']), userController.auth, collectController.unCollectSite);
 
-    router.get('/search', function *(next) {
-        if (this.query.type) {
-            yield searchService.getSiteByType.call(this);
-        } else {
-            yield searchService.getFamous.call(this);
-        }
-    });
+    router.post('/sites', blankAuth(['type'],['limit', 'page']), siteController.getSiteByType);
 
-    router.get('/:site/conf', function *(next) {
-        yield _controller.getSiteConf.call(this);
-    });
+    router.use('/site', blankAuth(['site']), siteController.siteEntrance);
 
-    router.post('/user/login', function*(next) {
-        yield loginService.login.call(this);
-    });
+    router.get('/site/conf', siteController.getSiteConf);
 
-    router.post('/user/session', function*(next) {
-        yield loginService.session.call(this);
-    });
-
-    router.post('/user/sign', function*(next) {
-        yield loginService.sign.call(this);
-    });
-
-    router.post('/user/collect', loginService.auth, function *(next) {
-        yield collectService.getCollections.call(this);
-    });
-
-    router.post('/user/collected', loginService.auth, function *(next) {
-        yield collectService.judgeCollected.call(this);
-    });
-
-    router.post('/user/sitefocus', loginService.auth, function *(next) {
-        yield collectService.getSitefocused.call(this);
-    });
-
-    router.post('/user/sitefocused', loginService.auth, function *(next){
-        yield collectService.judgeSiteFocused.call(this);
-    });
-
-    router.post('/collect', loginService.auth, function *(next) {
-        yield collectService.collected.call(this);
-    });
-
-    router.delete('/collect', loginService.auth, function *() {
-        yield collectService.unCollected.call(this);
-    });
-
-    router.post('/collect', methodAuth('delete'), loginService.auth, function *(next) {
-        yield collectService.unCollected.call(this);
-    });
-
-    router.post('/:site/collect', loginService.auth, function *() {
-        yield collectService.collectedSite.call(this);
-    });
-
-    router.delete('/:site/collect', loginService.auth, function *() {
-        yield collectService.unCollectedSite.call(this);
-    });
-
-    router.post('/:site/collect', methodAuth('delete'), loginService.auth, function *(next) {
-        yield collectService.unCollectedSite.call(this);
-    });
-
-    router.get('/:site/user/:name', function *(next) {
+    router.get('/site/user/:name', function *(next) {
 
     });
 
-    router.get('/:site/user', function*(next) {
+    router.get('/site/user', function*(next) {
 
     });
 
-    router.post('/:site/user', function *(next) {
+    router.post('/site/user', function *(next) {
 
     });
 
-    router.post('/:site/user/:name', methodAuth('delete'), function *(next) {
+    router.post('/site/user/:name', methodAuth('delete'), function *(next) {
     });
 
-    router.post('/:site/user/:name', methodAuth('put'), function *(next) {
+    router.post('/site/user/:name', methodAuth('put'), function *(next) {
 
     });
 
-    router.get('/:site/themes', function *(next) {
-        yield _controller.getThemes.call(this);
-    });
-
-    router.get('/:site/theme/:id', function *(next) {
-        yield _controller.getTheme.call(this);
-    });
+    router.get('/site/theme', blankAuth(['theme', 'limit', 'page']), siteController.getSiteContentBySiteId);
 
 
-    router.get('/:site/topic/:id', function *(next) {
-        yield _controller.getTopic.call(this);
-    });
+    router.get('/site/topic/:id', blankAuth(['site'], null, ['id']), siteController.getSiteContentByContentId);
 
-    router.post('/:site/topic', function *(next) {
+    router.post('/site/topic', function *(next) {
         yield _controller.publishTopic.call(this);
     });
 
-    router.post('/:site/topic/:id', methodAuth('delete'), function *(next) {
+    router.post('/site/topic/:id', methodAuth('delete'), function *(next) {
 
     });
 
-    router.post('/:site/topic/:id', methodAuth('put'), function *(next) {
+    router.post('/site/topic/:id', methodAuth('put'), function *(next) {
 
     });
 
-    router.post('/:site/comment', function *(next) {
+    router.post('/site/comment', function *(next) {
         yield _controller.publishComment.call(this);
     });
 
-    router.post('/:site/comment/:id', methodAuth('delete'), function *(next) {
+    router.post('/site/comment/:id', methodAuth('delete'), function *(next) {
 
     });
 
-    router.post('/:site/comment/:id', methodAuth('put'), function *(next) {
+    router.post('/site/comment/:id', methodAuth('put'), function *(next) {
 
     });
+
+    router.post('/user/login', userController.login);
+
+    router.post('/user/session', userController.session);
+
+    router.post('/user/sign', userController.sign);
+
+    router.post('/user/collect', userController.auth, function *(next) {
+        yield collectService.getCollections.call(this);
+    });
+
+    router.post('/user/collected', userController.auth, function *(next) {
+        yield collectService.judgeCollected.call(this);
+    });
+
+    router.post('/user/sitefocus', userController.auth, function *(next) {
+        yield collectService.getSitefocused.call(this);
+    });
+
+    router.post('/user/sitefocused', userController.auth, function *(next){
+        yield collectService.judgeSiteFocused.call(this);
+    });
+
+    router.post('/collect', userController.auth, function *(next) {
+        yield collectService.collected.call(this);
+    });
+
+    router.delete('/collect', userController.auth, function *() {
+        yield collectService.unCollected.call(this);
+    });
+
+    router.post('/collect', methodAuth('delete'), userController.auth, function *(next) {
+        yield collectService.unCollected.call(this);
+    });
+
+    router.get('/recommend/welcome', recommendController.getWelcomeContent)
 
     return router;
 }
