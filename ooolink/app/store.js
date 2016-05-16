ed6ae4f9a6bdce954992bdb7e8e2b558f0d0525c
Rube <353371737@qq.com>
2016-05-17 02:47:17 +0800

    加入react-native-storage

--- ooolink/app/store.js
+++ ooolink/app/store.js
@@ -9,6 +9,7 @@
 
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers'
+import Storage from 'react-native-storage'
 import { createStore, applyMiddleware } from 'redux'
 
 const createStoreWithMiddleware = applyMiddleware(
@@ -19,21 +20,65 @@ export default function configureStore(initialState) {
     return createStoreWithMiddleware(rootReducer, initialState)
 }
 
-let global = {
-    appVersion: '0.0.1',
-    oooLinkToken: 'a27778cc4a545ac318a840d858b21cadafa8a4ea4ef59a13b08afbd1fe8a3478',
-    userName: 'rube'
-};
-
-export function getGlobal(key) {
-    "use strict";
-    if (key) {
-        return global[key];
+let storage = new Storage({
+    size: 6666,
+    defaultExpires: 1000*3600*24*30*6,
+    enableCache: true,
+    sync: {
+        appVersion(params){
+            let { resolve, reject } = params;
+            resolve('0.0.1');
+        },
+        isLogin(params){
+            let { resolve, reject } = params;
+            resolve(false);
+        },
+        loginBgImage(params){
+            let { resolve, reject } = params;
+            resolve('https://s-media-cache-ak0.pinimg.com/474x/fb/2e/a5/fb2ea5f431df6765f14632d644ca58b1.jpg');
+        }
     }
-    return global;
+});
+
+export function getGlobal(key, id, cb) {
+    "use strict";
+    if (!key || !id){
+        return;
+    }
+    let callback = null;
+    let obj = {
+        key
+    };
+    if (typeof id === 'function'){
+        callback = id;
+    } else if (typeof cb === 'function') {
+        callback = cb;
+        obj.id = id;
+    }
+    storage.load(obj).then(ret=>{
+        callback && callback(ret);
+    }).catch(err=>{
+        console.log(`null rs key:${key} id:${id}`);
+        if (err){
+            console.log(err);
+        }
+        callback(undefined);
+    });
 }
 
-export function setGlobal(key, value) {
+export function setGlobal(key, value, expires) {
     "use strict";
-    global[key] = value;
+    let id;
+    if (typeof key === 'object'){
+        id = key.id;
+        key = key.key;
+    }
+    let obj = {key, rawData: value};
+    id && (obj.id = id);
+    expires && (obj.expires = expires);
+    storage.save(obj);
 }
+
+
+
+
